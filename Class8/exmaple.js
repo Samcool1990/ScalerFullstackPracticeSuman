f4a5a91e5cd92d4bbc69cf5bccbed5e1ece72d37
Suman Pathak <suman.pathak@smartims.com>
2024-04-18 16:29:44 +0530

    Class9

---
+++ Class8/exmaple.js
@@ -0,0 +1,55 @@
+console.log("Hello World!")
+
+var box;
+box = 1;
+console.log(box)
+var box1;
+box1 = 'Chilli'
+console.log(box1)
+var box2;
+box2 = ' ';
+console.log(box2)
+
+
+105-85/105*100
+console.log(105-85/105*100)
+console.log(((105-85)/105)*100)
+
+console.log(10%3)
+
+
+var quantity;
+quantity = 2;
+console.log(quantity)
+console.log(quantity++)
+console.log(++quantity)
+
+console.log(1 == 1);
+console.log("Apple" == "Apple");
+console.log(1 == "1");
+console.log(0 == false);
+console.log(1 == true);//Autocast (compare value & datatype)
+
+
+var price = 340;//Condition
+price < 300 ? console.log("Add more items to place order!!!!") : console.log("Buy more to save more!!!")
+
+
+console.log( typeof quantity)
+console.log( typeof price)
+
+function my_func(param1){
+    
+}
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+
+console.log(true && "apple");
+console.log(true && 1 && "apple");
+console.log(true && 1 && "apple","banana");
+console.log(true && 0 && "apple" && false && "banana");//Dadaji concept
+console.log(true || false);
+console.log(false ||  "apple" || true );//Dadiji concept
