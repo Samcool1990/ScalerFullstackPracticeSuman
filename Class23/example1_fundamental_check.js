ed0e15a90184202700af8aefcf5d3fb3b4f1eb99
Suman Pathak <suman.pathak@smartims.com>
2024-05-23 08:49:30 +0530

    Class completed

---
+++ Class23/example1_fundamental_check.js
@@ -0,0 +1,67 @@
+// console.log('1');
+// console.log('2');
+// console.log('3');
+
+// for (var i=0; i < 3; i++) {
+//     console.log('4');
+// }
+
+// console.log('5');
+
+// console.log('1');
+
+// while(true) {
+//     console.log('2')
+// }
+
+// console.log('3');
+
+
+// console.log('1');
+
+// function getTotal(a,b) {
+//     console.log('2');
+//     return a+b;
+// }
+
+// function sum(a,b) {
+//     console.log('3');
+//     return getTotal(a,b);
+// }
+
+// sum(1,2);
+// console.log('4');
+
+// console.log('1');
+
+// setTimeout(() => {
+//     console.log('2');
+// },1000);
+
+// setTimeout(() => {
+//     console.log('3');
+// });
+
+// for (var i = 0; i < 3; i++) {
+//     console.log('4');
+// }
+
+// console.log('5');
+
+
+// setTimeout(() => {
+//     console.log('6');
+// }, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
