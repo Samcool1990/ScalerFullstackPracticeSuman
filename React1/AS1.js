1e7c1d197a7204ce8ea4c28c39a97fa8236c8d83
Suman Pathak <suman.pathak@smartims.com>
2024-06-12 19:44:44 +0530

    js last assignment done

---
+++ React1/AS1.js
@@ -0,0 +1,8 @@
+function Greeting(obj) {
+    // write the logic here to receive the props
+      const name = 'Jasbir';
+    return <h1>Hello {name} Thanks Babel :) is {age} years old and works as {occupation}</h1>;
+  }
+  
+  export default Greeting;
+  
