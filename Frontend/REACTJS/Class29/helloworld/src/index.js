751fd9f112354eb1433745f7279674d396617ed3
Suman Pathak <suman.pathak@smartims.com>
2024-07-30 07:15:37 +0530

    react class 29 updated

--- Frontend/REACTJS/Class29/helloworld/src/index.js
+++ Frontend/REACTJS/Class29/helloworld/src/index.js
@@ -1,43 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const Detail = () => {
-  return (
-    <ul>
-        <li>Detail 1</li>
-        <li>Detail 2</li>
-        <li>Detail 3</li>
-        <li>Detail 4</li>
-      </ul>
-  )
-}
-
-const Heading = (props) => {
-  return (
-    <h1>YooHoo React! - {props.name} {props.surname}</h1>
-  )
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const Home = () => {
-  return (
+  return(
     <div>
-      <Heading name="Payal" surname="Butala"/>
-      <Heading name="Gyanendra" />
-      <Heading name="Pathak" />
-      <p>This is detail section</p>
-      <Detail />
-      <button>Confirm</button>
+      <h1>Hello World!!</h1>
+        <p>This is a p tag</p>        
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+        <button>Submit</button>
     </div>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Home />
-);
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>
+    <Home/> 
+    
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
